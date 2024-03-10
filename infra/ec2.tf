fa5bd8353b71829c92c424de575ba77f03c27df9
127895233-myseneca <rvsharma2@myseneca.ca>
2024-03-10 00:54:39 -0500

    adding application config files

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -43,13 +43,13 @@ resource "aws_instance" "k8s" {
     project = "clo835"
   }
 
-  key_name                = "week5"
+  key_name                = "t3minstancekey"
   monitoring              = true
   disable_api_termination = false
   ebs_optimized           = true
 }
 
 resource "aws_key_pair" "k8s" {
-  key_name   = "week5"
-  public_key = file("${path.module}/week5.pub")
+  key_name   = "t3minstancekey"
+  public_key = file("${path.module}/t3minstancekey.pub")
 }
